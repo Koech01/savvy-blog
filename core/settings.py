ad6443c46739eeed595766f531f1ba0d148188f8
Koech01 <01koech@gmail.com>
2025-06-14 12:21:57 +0300

    Updating cors.

--- core/settings.py
+++ core/settings.py
@@ -128,7 +128,7 @@ CORS_ALLOW_HEADERS = [
     "user-agent", "x-csrftoken", "x-requested-with",
 ]
 
-CORS_ALLOWED_ORIGINS = [ "http://127.0.0.1:5173", "http://localhost:5173" ]
+CORS_ALLOWED_ORIGINS = [ "http://127.0.0.1:5173", "http://localhost:5173", "http://127.0.0.1:8000", "http://localhost:8000" ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
